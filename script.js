666caef38324fd7782ddbdb35395bebc1a07f55d
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-31 17:37:15 -0300

    Add logic to add values and operate
    Hitting any opperator when an opperator has already been assigned
    will trigger the current evaluation. Enter button also works.
    Floating point numbers are rounded to up to 2 digits in order.
    to not overflow the screen
    
    Code needs further readability improvements, with new functions
    and new function names, especially on the buttons event listener
    Zeros at left should also be replaced with other digits, but
    they are stacking.
    
    A suggestion for a new feature on future versions is a backspace
    button to undo the button hit (digit or operator)

--- script.js
+++ script.js
@@ -1,33 +1,110 @@
+let firstValue = '';
+let secondValue = '';
+let operator;
+let changingFirstValue = true;
+let changingSecondValue = false;
+
+function updatePanel(){
+  
+  if (changingFirstValue){
+  panel.textContent = firstValue;
+  } else {
+  panel.textContent = firstValue + operator + secondValue;
+  }
+  
+}
+
 function operate(a,operator,b){
+  
     switch(operator){
-        case "/": return divide(a,b);
-        case "*": return multiply(a,b);
-        case "+": return sum(a,b);
-        case "-": return subtract(a,b);
+            case "*":
+              firstValue = multiply(a,b);
+              break;
+              
+            case "/":
+              firstValue = divide(a,b);
+              break;
+              
+            case "+":
+              firstValue = sum(a,b);
+              break;
+              
+            case "-":
+              firstValue = subtract(a,b);
+              break;
     }
+  
 }
 
 function sum(a,b){
-    return a+b;
+    let added = a+b;
+    added = Math.round(added * 100) / 100;
+    return added;
 }
 
 function subtract (a,b){
-    return a-b;
+    let subtracted = a-b;
+    divided = Math.round(subtracted * 100) / 100;
+    return subtracted;
 }
 
 function divide (a,b){
-    return a/b;
+    let divided = a/b;
+    divided = Math.round(divided * 100) / 100;
+    return divided;
+
 }
 
 function multiply (a,b){
-    return a*b;
+    let multiplied = a*b;
+    multiplied = Math.round(multiplied * 100) / 100;
+    return multiplied;
 }
 
 const buttons = document.querySelectorAll("button");
 const panel = document.querySelector("#panel");
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        panel.textContent = panel.textContent + button.textContent;
+    button.addEventListener("click", (e) => {
+        
+        if(e.target.id == "clear"){
+          changingFirstValue = true;
+          firstValue = '';
+          secondValue = '';
+          panel.textContent = 0;
+          return;
+        }
+      
+        if(e.target.className=="operator"){
+          if(changingFirstValue === false){
+          operate(Number(firstValue),operator,Number(secondValue));
+          secondValue = '';
+          operator = button.textContent;
+          updatePanel()
+          return;
+          }
+          
+          changingFirstValue = false;
+          operator = button.textContent;
+          updatePanel();
+          return;
+        } 
+      
+        if(e.target.id=="enterButton"){
+          operate(Number(firstValue),operator,Number(secondValue));
+          changingFirstValue = true;
+          secondValue = '';
+          updatePanel()
+          return;
+        }
+    
+      
+        if(changingFirstValue){
+          firstValue = String(firstValue) + button.textContent;
+        } else {
+          secondValue = String(secondValue) + button.textContent;
+        }
+      
+        updatePanel();
     });
 });
